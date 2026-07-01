11e7909f2b75c2c818b3fdb89b6f09f024871d15
minasreeves-bit <mina.sreeves@gmail.com>
2026-07-01 08:31:22 -0700

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-function toggleMenu(){document.getElementById('menu').classList.toggle('open')}
-document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>document.getElementById('menu').classList.remove('open')));
